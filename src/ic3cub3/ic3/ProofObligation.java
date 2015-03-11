2902dbe38b002b8bb79cd8faa2b30f82908dc9db
Rick Hindriks <rick.hindriks@gmail.com>
2015-03-11 15:02:57 +0100

    Store proof traces in proof obligation class

--- src/ic3cub3/ic3/ProofObligation.java
+++ src/ic3cub3/ic3/ProofObligation.java
@@ -3,6 +3,8 @@ package ic3cub3.ic3;
 import ic3cub3.plf.cnf.Cube;
 import ic3cub3.runner.Runner;
 
+import java.util.*;
+
 /**
  * Class representing an obligation to prove that a given CTI should not hold be
  * reachable within level+1 steps
@@ -11,13 +13,19 @@ import ic3cub3.runner.Runner;
 public class ProofObligation implements Comparable<ProofObligation> {
 	private final Cube CTI;
 	private final int level;
+	private final ProofObligation parent;
 
 	public ProofObligation(Cube CTI, int level) {
+		this(CTI,level,null);
+	}
+	
+	public ProofObligation(Cube CTI, int level, ProofObligation parent){
 		if (Runner.VERBOSE > 0)
 			System.out.println(String.format(
 					"New proof obligation: %s at level %d", CTI, level));
 		this.CTI = CTI;
 		this.level = level;
+		this.parent = parent;
 	}
 
 	public Cube getCTI() {
@@ -36,5 +44,21 @@ public class ProofObligation implements Comparable<ProofObligation> {
 	@Override
 	public String toString() {
 		return String.format("ProofObligation[%s,%d]",getCTI(),getLevel());
+	}
+	
+	public ProofObligation getParent() {
+		return parent;
+	}
+	
+	public LinkedList<ProofObligation> getProofTrace(){
+		LinkedList<ProofObligation> result;
+		if(parent==null){
+			result = new LinkedList<>();
+			result.add(this);
+		}else{
+			result = new LinkedList<>(parent.getProofTrace());
+			result.addFirst(this);
+		}
+		return result;
 	}
 }
