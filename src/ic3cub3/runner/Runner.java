10086ed35673b1afa629b7bbb2302dae011845b3
Rick Hindriks <rick.hindriks@gmail.com>
2015-04-18 22:20:51 +0200

    Provide a meaningful runner (run each testcase once) Make the run target depend on the build target

--- src/ic3cub3/runner/Runner.java
+++ src/ic3cub3/runner/Runner.java
@@ -13,29 +13,12 @@ public class Runner {
 	public static int VERBOSE = 0;
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		/*ParserHelper ph = new ParserHelper();
-		ProblemSet problems = ph.parse(new File("src/ic3cub3/rersproblems/problemtest.c"));
-		
-		System.out.println("Starting with model checking...");
-		SATSolver solver = new SAT4J();
-		IC3 ic3 = new IC3(solver);
-		for(int i = 0;i<problems.getProperties().size();i++){
-			System.out.println("Checking property "+(i+1));
-			List<ProofObligation> result = ic3.check(problems.getInitial(), problems.getTransition(), problems.getProperties().get(i).getProperty(), problems.getProperties().get(i).getNegatedProperty());
-			result.stream().map(ProofObligation::getCTI).map(ph.getGenerator()::convertState).forEach(System.out::println);
-			System.exit(0);
-		}
-		
-		System.out.println("Done");*/
-		
 		ProblemSet[] problems = new ProblemSet[]{new IC3WMIM_1(), new IC3WMIM_2(), new ReachableBadState()};
 		SATSolver solver = new SAT4J();
 		IC3 ic3 = new IC3(solver);
 		for(ProblemSet ps:problems){
 			long time = System.currentTimeMillis();
-			for(int i = 0;i<1000;i++){
-				ps.check(ic3);
-			}
+			ps.check(ic3);
 			System.out.println(String.format("Time needed: %dms",
 					System.currentTimeMillis() - time));
 		}
