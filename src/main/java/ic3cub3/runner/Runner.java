23493fb16baa251ae9e486f08257b647e0aa8e81
Rick Hindriks <rick.hindriks@gmail.com>
2015-06-27 12:37:30 +0200

    clean up main runner

--- src/main/java/ic3cub3/runner/Runner.java
+++ src/main/java/ic3cub3/runner/Runner.java
@@ -4,8 +4,7 @@ import ic3cub3.ic3.IC3;
 import ic3cub3.rersparser.ConcreteRersParser;
 import ic3cub3.rersparser.ParserHelper;
 import ic3cub3.sat.SAT4J;
-import ic3cub3.sat.SATSolver;
-import ic3cub3.tests.ProblemSet;
+import ic3cub3.tests.actual.IC3WMIM_1;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,23 +19,13 @@ public class Runner {
 	public static int VERBOSE = 1;
 
 	public static void main(String[] args) throws IOException {
-        System.out.println("cores: "+Runtime.getRuntime().availableProcessors());
+		IC3 ic3 = new IC3(new SAT4J());
 		ParserHelper ph = new ParserHelper(new ConcreteRersParser());
-		ph.parse(new File("src/main/java/ic3cub3/rersproblems/Problem1/Problem1.c"));
-        ph.check(new IC3(new SAT4J()));
+		ph.parse(new File("src/main/java/ic3cub3/rersproblems/problemtest.c"));
+		//ph.parse(new File("src/main/java/ic3cub3/rersproblems/Problem1/Problem1.c"));
+        ph.check(ic3);
 
-		//System.exit(0);
-		//ProblemSet[] problems = new ProblemSet[]{new IC3WMIM_1(), new IC3WMIM_2(), new ReachableBadState()};
-        ProblemSet[] problems = new ProblemSet[]{ph.getGenerator().getProblemSet()};
-		SATSolver solver = new SAT4J();
-		IC3 ic3 = new IC3(solver);
-		for(ProblemSet ps:problems){
-            printv(() -> "Processing problem set: "+ps,0);
-			long time = System.currentTimeMillis();
-			ps.check(ic3);
-			System.out.println(String.format("Time needed: %dms",
-					System.currentTimeMillis() - time));
-		}
+		new IC3WMIM_1().check(ic3);
 	}
 	
 	public static void printv(Supplier<Object> s, int minverbosity){
